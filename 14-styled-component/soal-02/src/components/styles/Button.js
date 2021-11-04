8b8f2e3fb5a1ac812ec86d5af84dcb20cbbfce98
jesslynauryn <88038899+jesslynauryn@users.noreply.github.com>
2021-11-04 23:43:57 +0700

    Update Button.js

--- 14-styled-component/soal-02/src/components/styles/Button.js
+++ 14-styled-component/soal-02/src/components/styles/Button.js
@@ -3,13 +3,17 @@ import styled from "styled-components";
 
 const StyledButton = styled.button`
   cursor: pointer;
-  padding: 0.5em 1.5em;
+  padding: 0.5em 10em;
   font-family: sans-serif;
   font-size: 1.25em;
   border-color: ${(props) => (props.outline ? "lightblue" : "white")};
   border-width: 1px;
   border-style: solid;
   width: ${(props) => (props.fullWidth ? "100%" : "auto")};
+  border-radius: 4px;
+  margin-top: 3em;
+  background-color: lightblue;
+  color: white;
   &:hover {
     border-color: ${(props) =>
       props.outline ? "lightblue" : "cornflowerblue"};
